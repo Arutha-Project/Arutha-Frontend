67ca8286368b6272d08a1de8ed2615bc8e12ca68
it21261350 <it21261350@my.sliit.lk>
2025-03-13 05:58:52 +0530

    Object Identifier interface development

---
+++ src/component/pages/objectIdentifierPage/objectIdentifierPageStyle.ts
@@ -0,0 +1,76 @@
+export const mainLayoutContainerOI: React.CSSProperties = {
+    display: 'flex',
+    backgroundImage: 'url(src/assets/images/Object_Identifier_background_image.jpg)',
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh',
+  };
+
+export const Container1: React.CSSProperties = {
+    backgroundColor: 'white',
+    padding: '15px',
+    borderRadius: '15px',
+    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+    maxWidth: '1000px',
+    width: '1000px',
+    margin: 'auto',
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    gap: '20px',
+  };
+
+export const videoStyle: React.CSSProperties = {
+    width: '100%',
+    maxWidth: '1000px',
+    height: '500px',
+    borderRadius: '10px',
+    border: '2px solid #ddd',
+    objectFit: 'cover' 
+  };
+
+export const startButtonDevTagStyle: React.CSSProperties = {
+    display: 'flex', 
+    flexDirection: 'column', 
+    alignItems: 'center', 
+    marginBottom: '20px'
+  };
+
+export const startButtonStyle: React.CSSProperties = {
+    padding: '10px 16px',
+    borderRadius: '8px',
+    background: '#007bff',
+    color: 'white',
+    border: 'none',
+    cursor: 'pointer',
+    fontSize: '16px',
+  };
+
+export const nextButtonStyle: React.CSSProperties = {
+    padding: '10px 16px',
+    borderRadius: '8px',
+    background: '#007bff',
+    color: 'white',
+    border: 'none',
+    cursor: 'pointer',
+    fontSize: '16px',
+    alignItems: 'center',
+    marginLeft: '20px'
+  };
+
+export const leftShowingData: React.CSSProperties = {
+    textAlign: "center",
+    marginTop: "20px", 
+    marginBottom: "100px"
+  };
+
+export const processingGif: React.CSSProperties = {
+    marginTop: '15px', 
+    fontSize: '18px', 
+    fontWeight: 'bold', 
+    color: 'blue'
+  };
