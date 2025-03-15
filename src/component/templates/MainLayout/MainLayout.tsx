35ac10e2b6da621e0d92d7aab698a5c6e39bdb26
ushandilusha <ushandilusha10@gmail.com>
2025-03-15 10:38:09 +0530

    fixed main layout

--- src/component/templates/MainLayout/MainLayout.tsx
+++ src/component/templates/MainLayout/MainLayout.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Layout, Spin } from "antd";
 import { Content } from "antd/es/layout/layout";
-import { SideMenu } from "../../organisms";
+import SideMenu from "../../organisms/sideMenu/SideMenu";
 
 interface MainLayoutProps {
   children: React.ReactNode;
