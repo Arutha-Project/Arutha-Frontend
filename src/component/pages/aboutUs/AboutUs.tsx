9c4d5de24f346b954a2a0043383d3186811cccac
OvindaNamal <j.m.ovinda@gmail.com>
2025-03-15 21:05:20 +0530

    Missing part added - about us page side menu - not redirect to self study page

--- src/component/pages/aboutUs/AboutUs.tsx
+++ src/component/pages/aboutUs/AboutUs.tsx
@@ -1,18 +1,21 @@
 import { useTranslation } from 'react-i18next';
 import {  contentContainer, mainLayoutContainer } from './AboutUsStyle';
+import { MainLayout } from "../../templates";
 
 const AboutUs: React.FC = () => {
     const {t} = useTranslation();
 
     return (
-        <div style={mainLayoutContainer}>
-        <div style={contentContainer}>
-            <h1>{t("aboutUs")}</h1>
-            <p>{t("aboutUsContent")}</p>
-            <p>{t("aboutUsContent2")}</p><br/>
-            <p><b>{t("aboutUsContent3")}</b></p>
-        </div>
-        </div>
+        <MainLayout>
+            <div style={mainLayoutContainer}>
+                <div style={contentContainer}>
+                    <h1>{t("aboutUs")}</h1>
+                    <p>{t("aboutUsContent")}</p>
+                    <p>{t("aboutUsContent2")}</p><br/>
+                    <p><b>{t("aboutUsContent3")}</b></p>
+                </div>
+            </div>
+        </MainLayout>
     );
 };
 
