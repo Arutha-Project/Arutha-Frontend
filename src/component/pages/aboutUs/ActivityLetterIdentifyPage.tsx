55c0532a683993544d1f9bb238af55741229ebea
Isharam99 <IT21183904@my.sliit.lk>
2025-05-25 23:32:25 +0530

    file changes

--- src/component/pages/aboutUs/ActivityLetterIdentifyPage.tsx
+++ src/component/pages/aboutUs/ActivityLetterIdentifyPage.tsx
@@ -13,7 +13,7 @@ import {
   rightSideSinhala,
   videoContainer,
   videoStyle,
-} from "./ActivityLetterIdentifyPageStyle";
+} from "../activityLetterIdentifyPage/ActivityLetterIdentifyPageStyle";
 import { MainLayout } from "../../templates";
 import { useTranslation } from "react-i18next";
 import axios from "../../../services/axiosInstance";
